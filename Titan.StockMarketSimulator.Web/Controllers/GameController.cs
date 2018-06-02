f020b33b93d9d95e4a09c4b897d3253bf384dde6
malaka7fernando@gmail.com <cmfernando654@gmail.com>
2018-06-02 22:41:11 +0530

    Your Commit

---
+++ Titan.StockMarketSimulator.Web/Controllers/GameController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Titan.StockMarketSimulator.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult GameBoard(string txtUsrname)
+        {
+            //txtUsrname
+            ViewBag.Usrname = txtUsrname;
+            return View();
+        }
+
+    }
+}
---
+++ Titan.StockMarketSimulator.Web/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Titan.StockMarketSimulator.Web.WebApiApplication" Language="C#" %>
