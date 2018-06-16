812f5a950240b47f736bfb39f6c330eb65bb7166
Oshada Ekanayake <ekanayake.osh@gmail.com>
2018-06-16 09:39:18 +0530

    adding business logics project

---
+++ Titan.StockMarketSimulator.GameLogic/Entities/GameContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Titan.StockMarketSimulator.GameLogic.Entities
+{
+    public static class GameContainer
+    {
+        private static List<Player> players = new List<Player>();
+
+        public static List<Player> Players { get => players; set => players = value; }
+    }
+}
